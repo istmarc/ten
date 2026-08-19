d7bf9a19c22bb02cabc3ab6ed64b79df532093d1
Marc <mnawman@proton.me>
2026-08-19 15:47:41 +0100

    [ten/maths] Simulation using inverse transform method

---
+++ ten/maths/sim.hxx
@@ -0,0 +1,22 @@
+#ifndef TEN_MATHS_SIM_HXX
+#define TEN_MATHS_SIM_HXX
+
+#include <functional>
+
+#include <ten/random>
+#include <ten/types.hxx>
+
+namespace ten {
+// Inverse transform method
+template <typename T>
+tensor<T> inv_transform(std::function<T(T)> Finv, const std::size_t size) {
+  tensor<T> u = rand_unif<T>({size});
+  tensor<T> x({size});
+  for (size_t i = 0; i < size; i++) {
+    x[i] = Finv(u[i]);
+  }
+  return x;
+}
+} // namespace ten
+
+#endif
