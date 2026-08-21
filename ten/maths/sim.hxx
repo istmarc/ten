d878932e86155a1a560d47e50c6ec7ecb9caa836
Marc <mnawman@proton.me>
2026-08-21 14:11:07 +0100

    Small updates for dist, now included only with random

--- ten/maths/sim.hxx
+++ ten/maths/sim.hxx
@@ -12,7 +12,7 @@ template <typename T>
 tensor<T> inv_transform(std::function<T(T)> Finv, const std::size_t size) {
   tensor<T> u = rand_unif<T>({size});
   tensor<T> x({size});
-  for (size_t i = 0; i < size; i++) {
+  for (std::size_t i = 0; i < size; i++) {
     x[i] = Finv(u[i]);
   }
   return x;
