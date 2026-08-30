124d0bbc28c61a0eef8e7a1fd6428bd1831f6911
Marc Nawman <mnawman@proton.me>
2026-08-30 11:53:15 +0200

    Update cholesky example

--- cpp/cholesky.cxx
+++ cpp/cholesky.cxx
@@ -6,7 +6,7 @@ int main() {
   using namespace ten;
   auto x = range<float>({4, 4});
   std::cout << x << std::endl;
-  auto [q, r] = linalg::cholesky(x);
-  std::cout << q << std::endl;
-  std::cout << r << std::endl;
+  auto [l, u] = linalg::cholesky(x);
+  std::cout << l << std::endl;
+  std::cout << u << std::endl;
 }
