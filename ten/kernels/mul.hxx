670b6cbde7c7945835792506e83b8261e1001412
Marc Nawman <mnawman@proton.me>
2026-09-12 18:10:15 +0200

    Fix matrix multiplication

--- ten/kernels/mul.hxx
+++ ten/kernels/mul.hxx
@@ -12,7 +12,6 @@ template <Tensor A, Tensor B, Tensor C> static void mul(A &&a, B &&b, C &c) {
   const std::size_t rankc = c.rank();
   /// Matrix vector multiplication
   if (ranka == 2 && rankb == 1 && rankc == 1) {
-    // TODO Test with transposed and no transposed
     std::size_t m = a.dim(0);
     std::size_t n = a.dim(1);
     using blas::transop;
@@ -24,24 +23,35 @@ template <Tensor A, Tensor B, Tensor C> static void mul(A &&a, B &&b, C &c) {
     const std::size_t incc = 1;
     if (transa == transop::no) {
       ::ten::kernels::blas::gemv(transa, m, n, T(1.), a.data(), lda, b.data(),
-                               incb, T(0.), c.data(), incc);
+                                 incb, T(0.), c.data(), incc);
     } else {
       ::ten::kernels::blas::gemv(transa, n, m, T(1.), a.data(), lda, b.data(),
-                               incb, T(0.), c.data(), incc);
+                                 incb, T(0.), c.data(), incc);
     }
   } else if (ranka == 2 && rankb == 2 && rankc == 2) {
     // Multiply two dense matrices
-    std::size_t m = a.dim(0);
-    std::size_t k = a.dim(1);
-    std::size_t n = b.dim(1);
+    // [m,k] * [k,n] -> [m, n]
     using blas::transop;
     using T = typename std::remove_cvref_t<A>::value_type;
     const transop transa = (a.is_transposed() ? transop::trans : transop::no);
     const transop transb = (b.is_transposed() ? transop::trans : transop::no);
-    const std::size_t lda = (transa == transop::no ? m : k);
-    const std::size_t ldb = (transa == transop::no ? k : n);
-    ::ten::kernels::blas::gemm(transa, transb, m, n, k, T(1.), a.data(), lda,
-                               b.data(), ldb, T(0.), c.data(), m);
+    if (transa == transop::no && transb == transop::no) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(0), b.dim(1), a.dim(1),
+                                 T(1.), a.data(), a.dim(0), b.data(), b.dim(0),
+                                 T(0.), c.data(), a.dim(0));
+    } else if (transa == transop::no && transb == transop::trans) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(0), b.dim(0), b.dim(1),
+                                 T(1.), a.data(), a.dim(0), b.data(), b.dim(0),
+                                 T(0.), c.data(), a.dim(0));
+    } else if (transa == transop::trans && transb == transop::no) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(1), b.dim(1), a.dim(0),
+                                 T(1.), a.data(), a.dim(0), b.data(), b.dim(0),
+                                 T(0.), c.data(), a.dim(1));
+    } else if (transa == transop::trans && transb == transop::trans) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(1), b.dim(0), a.dim(0),
+                                 T(1.), a.data(), a.dim(0), b.data(), b.dim(0),
+                                 T(0.), c.data(), a.dim(1));
+    }
   } else {
     std::cerr << "mul Incompatible dimensions.\n";
   }
@@ -56,16 +66,28 @@ static void mul_add(const T alpha, A &&a, B &&b, const T beta, C &c) {
   const std::size_t rankb = b.rank();
   const std::size_t rankc = c.rank();
   if (ranka == 2 && rankb == 2 && rankc == 2) {
-    std::size_t m = a.dim(0);
-    std::size_t k = a.dim(1);
-    std::size_t n = b.dim(1);
+    // Multiply two dense matrices
+    // alpha * [m,k] * [k,n] + beta * [m, n]-> [m, n]
     using blas::transop;
     const transop transa = (a.is_transposed() ? transop::trans : transop::no);
     const transop transb = (b.is_transposed() ? transop::trans : transop::no);
-    const std::size_t lda = (transa == transop::no ? m : k);
-    const std::size_t ldb = (transa == transop::no ? k : n);
-    ::ten::kernels::blas::gemm(transa, transb, m, n, k, alpha, a.data(), lda,
-                               b.data(), ldb, beta, c.data(), m);
+    if (transa == transop::no && transb == transop::no) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(0), b.dim(1), a.dim(1),
+                                 alpha, a.data(), a.dim(0), b.data(), b.dim(0),
+                                 beta, c.data(), a.dim(0));
+    } else if (transa == transop::no && transb == transop::trans) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(0), b.dim(0), b.dim(1),
+                                 alpha, a.data(), a.dim(0), b.data(), b.dim(0),
+                                 beta, c.data(), a.dim(0));
+    } else if (transa == transop::trans && transb == transop::no) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(1), b.dim(1), a.dim(0),
+                                 alpha, a.data(), a.dim(0), b.data(), b.dim(0),
+                                 beta, c.data(), a.dim(1));
+    } else if (transa == transop::trans && transb == transop::trans) {
+      ::ten::kernels::blas::gemm(transa, transb, a.dim(1), b.dim(0), a.dim(0),
+                                 alpha, a.data(), a.dim(0), b.data(), b.dim(0),
+                                 beta, c.data(), a.dim(1));
+    }
   } else {
     std::cerr << "mul_add Incompatible dimensions.\n";
   }
