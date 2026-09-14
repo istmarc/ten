ee051719531cfb3a07944e4cc0431e7a762c2cf3
Marc Nawman <mnawman@proton.me>
2026-09-14 10:13:04 +0200

    Polynomial regression

---
+++ ten/learning/polyreg.hxx
@@ -0,0 +1,46 @@
+#ifndef TEN_LEARNING_POLYREG
+#define TEN_LEARNING_POLYREG
+
+#include <ten/linalg>
+#include <ten/tensor>
+
+namespace ten::ml {
+
+/// Polynomial regression
+template <typename T = float> class polyreg {
+private:
+  std::size_t _n;
+  tensor<T> _beta = tensor<T>::make_default();
+  tensor<T> _yhat = tensor<T>::make_default();
+
+public:
+  polyreg(std::size_t n) : _n(n) {}
+
+  void fit(tensor<T> &x, tensor<T> &y, bool fitted = true) {
+    if (x.rank() == 1) {
+      std::size_t m = x.size(0);
+      tensor<T> X({m, _n});
+      X.col(0) = T(1);
+      for (std::size_t i = 0; i < m; i++) {
+        X(i, 1) = x[i];
+      }
+      for (std::size_t i = 2; i < _n; i++) {
+        for (std::size_t j = 0; j < m; j++) {
+          X(j, i) = X(j, i - 1) * x[j];
+        }
+      }
+      _beta = linalg::lsqr(X, y);
+      if (fitted) {
+        _yhat = X * _beta;
+      }
+    }
+  }
+
+  tensor<T> coef() const { return _beta; }
+
+  tensor<T> fitted() const { return _yhat; }
+};
+
+} // namespace ten::ml
+
+#endif
